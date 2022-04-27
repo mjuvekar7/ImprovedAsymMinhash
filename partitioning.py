770046ef202a427e6f6b6bdb25f8eed94e6453a4
Mandar Juvekar <mjuvekar7@gmail.com>
2022-04-26 21:50:11 -0400

    Outline of partitioning methods.

---
+++ partitioning.py
@@ -0,0 +1,24 @@
+"""
+Partition into equal width partitions.
+Input: a dictionary of the form {set_id : set}, a positive integer nparts
+Output: a partition (list of dictionaries each of the form {set_id : set}) into
+        nparts parts such that each part has the same number of sets.
+"""
+def equalWidthPartition(sets, nparts):
+    pass # TODO
+
+
+"""
+Partition into equal depth partitions.
+Input: a dictionary of the form {set_id : set}, a positive integer nparts
+Output: an equal depth partition (same output format as above).
+"""
+def equalDepthPartition(sets, nparts):
+    pass # TODO
+
+"""
+TODO figure this out
+"""
+def ourPartitioningScheme(sets, nparts):
+    pass # TODO
+
