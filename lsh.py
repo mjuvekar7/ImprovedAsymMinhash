350c322d7e54c28f04dc74884c8b441f68632ffd
Mandar Juvekar <mjuvekar7@gmail.com>
2022-04-26 20:34:24 -0400

    Add LSH implementation (TODO test).

--- lsh.py
+++ lsh.py
@@ -1,32 +1,73 @@
-import minhash
+"""
+lsh.py
+Author: Mandar Juvekar
 
+A simple locality sensitive hashing implementation.
+Designed for use with minhash.
+"""
+
+"""
+An example band hash function that can be given to the LSHIndex constructor.
+"""
+def band_hash(signature):
+    return hash(str(band))
+
+
+
+"""
+An LSH index.
+"""
 class LSHIndex:
     """
-    Input: dictionary of form {set_name : minhash_signature}
+    Input: self.signatures should be of the form {set_name : minhash_signature}
     Output: LSH index, i.e. list of LSH_B dicts each of the form
     {bucket_id : set_id}.
     """
-    def build_lsh_index(signatures):
-        pass
+    def build_lsh_index(self):
+        index = [{}] * self.b
+        for set_id, sig in self.signatures.items():
+            for band_num in range(self.b):
+                band = sig[band_num * self.r : (band_num + 1) * self.r]
+                bucket = self.hashfunc(band)
+                if bucket in index[band_num]:
+                    index[band_num][bucket].add(set_id)
+                else:
+                    index[band_num][bucket] = {set_id}
+        return index
 
     """
     Get the LSH of a signature.
+    The output is a list specifying where each band gets hashed.
     """
     def lsh_hash(self, signature):
-        pass
+        lsh = []
+        for band_num in range(self.b):
+            band = signature[band_num * self.r : (band_num + 1) * self.r ]
+            bucket = self.hashfunc(band)
+            lsh.append(bucket)
+        return lsh
     
     """
     Given a query signature, find all candidates.
     """
     def query(self, q_signature):
-        pass
+        candidates = set()
+        buckets = self.lsh_hash(q_signature)
+        for band_num in range(self.b):
+            bucket = buckets[band_num]
+            if bucket in self.index[band_num]:
+                candidates = candidates.union(self.index[band_num][bucket])
+        return candidates
 
     """
     Constructor.
+    Signatures should be in the form { set_id : signature }.
+    lsh_hashfunc should be a function that takes a band and hashes to an integer.
     """
-    def __init__(self, lsh_b, lsh_r, lsh_hashfunc, signatures):
+    def __init__(self, lsh_b, lsh_r, lsh_hashfunc, lsh_signatures):
+        self.signatures = lsh_signatures
         self.b = lsh_b
         self.r = lsh_r
         self.hashfunc = lsh_hashfunc
-        self.index = build_lsh_index(signatures)
+        self.index = self.build_lsh_index()
 
--- minhash.py
+++ minhash.py
@@ -1,3 +1,14 @@
+"""
+minhash.py
+Author: Mandar Juvekar
+
+A simple minwise hashing implementation.
+"""
+
+from functools import reduce
+from math import inf
+from random import randint
+
 class MinHasher:
     """
     Generate hash function parameters. Return and save to file.
@@ -5,20 +16,42 @@ class MinHasher:
     function ax + b (mod c).
     """
     def generateParameters(outfile):
-        pass
+        PRIME = 2003
+        COUNT = 128
+
+        hashes = []
+        f = open(outfile, 'w')
+        for i in range(COUNT):
+            a = randint(1, PRIME - 1)
+            b = randint(1, PRIME - 1)
+            f.write(str(a) + ',' + str(b) + ',' + str(PRIME) + '\n')
+            hashes.append((a,b,PRIME))
+        f.close()
+        return hashes
 
     """
     Load hash function parameters from a file.
     File format: line of the form a,b,c for each hash.
     """
     def loadParameters(infile):
-        pass
+        return [(int(l[0]), int(l[1]), int(l[2])) for l in
+                   map(lambda x: x.split(','),
+                       filter(lambda x: x != '',
+                              open(infile, 'r').read().splitlines()))]
 
     """
     Get the vocabulary of a collection of sets.
+    A vocabulary is a dictionary that assigns each unique element a numeric ID.
     """
     def getVocab(sets):
-        pass
+        vocab_set = reduce(lambda x, y : x.union(y), sets)
+        vocab_count = 0
+        vocabulary = {}
+        for word in vocab_set:
+            vocabulary[word] = vocab_count
+            vocab_count += 1
+
+        return vocabulary
 
     """
     Given hash function parameters, get the hash of x.
@@ -31,7 +64,14 @@ class MinHasher:
     Generate the minhash signature of a set.
     """
     def minhash(self, in_set):
-        pass
+        signature = [inf] * len(self.hash_params)
+        for shingle in in_set:
+            rownum = self.vocabulary[shingle]
+            for i in range(len(self.hash_params)):
+                hashed_rownum = get_hash(rownum, self.hash_params[i])
+                if hashed_rownum < signature[i]:
+                    signature[i] = hashed_rownum
+        return signature
 
     """
     Constructor.
